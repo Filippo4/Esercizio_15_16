f41bd229007fc109982725315384841b1b4b1875
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-23 13:23:12 +0200

    aggiuto metodo con ricorsione

---
+++ Esercizio_1_pag_234/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Esercizio_1_pag_234
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int somma = 0;
+            Leggi(ref n, ref somma);
+            Console.WriteLine($"Hai inserito {n} numeri e la loro somma è {somma} ");
+            Console.ReadLine();
+        }
+
+        static void Leggi(ref int count , ref int somma)
+        {
+            Console.WriteLine("inserisci un numero");
+            int n = int.Parse(Console.ReadLine());
+            if (n == 0)
+                return;
+            else
+            {
+                count++;
+                somma += n;
+                Leggi(ref count, ref somma);
+            }
+        }
+    }
+}
