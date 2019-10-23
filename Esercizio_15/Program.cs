3eecf9efed9040a2993e87c2df4df395a80cc119
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-23 12:06:06 +0200

    Add project files.

---
+++ Esercizio_15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Esercizio_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire l'altezza");
+            double l1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inserire la base");
+            double l2 = double.Parse(Console.ReadLine());
+            if (l1==l2)
+            {
+                double perimetro = l1 * 4;
+                double area = l1 * l2;
+                Console.WriteLine("il quadrilatero è un quadrato!");
+                Console.WriteLine($"l'area è {area} il perimetro è {perimetro}!");
+            }
+            else
+            {
+                double perimetro = ( l1 + l2 ) * 2;
+                double area = l1 * l2;
+                Console.WriteLine("il quadrilatero è un rettangolo!");
+                Console.WriteLine($"l'area è {area} il perimetro è {perimetro}!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Esercizio_16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Esercizio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire il primo lato");
+            double l1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inserire il secondo lato");
+            double l2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inserire il terzo lato");
+            double l3 = double.Parse(Console.ReadLine());
+            if (l1 == l2 && l1==l3)
+            {
+                double perimetro = l1 * 3;
+                double h = (Math.Sqrt(3) / 2) * l1;
+                double area = (l1 * h) / 2;
+                Console.WriteLine("il triangolo è equilatero!");
+                Console.WriteLine($"l'area è {area} il perimetro è {perimetro}!");
+
+            }
+        }
+    }
+}
