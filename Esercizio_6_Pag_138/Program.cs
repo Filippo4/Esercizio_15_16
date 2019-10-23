274061425dc21f408d8c5ca940aa59f53ef900d1
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-23 12:46:21 +0200

    aggiuto metodo catatlan

---
+++ Esercizio_6_Pag_138/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Esercizio_6_Pag_138
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sconto;
+            double importoScontato;
+            Console.WriteLine("inserire l'importo della spesa!");
+            double importo = double.Parse(Console.ReadLine());
+            if (importo < 50)
+            {
+                sconto = (importo / 100) * 5;
+                
+            }
+            else if (importo > 50 && importo < 65)
+            {
+                sconto = (importo / 100) * 6;
+                
+            }
+            else if(importo > 65 && importo < 80)
+            {
+                sconto = (importo / 100) * 7;
+                
+            }
+            else
+            {
+                sconto = (importo / 100) * 10;
+               
+            }
+            importoScontato = importo - sconto;
+                Console.WriteLine($"lo sconto da applicare è di {sconto} l'importo da pagre è {importoScontato}");
+        }
+    }
+}
---
+++ Sfida_Pag_233/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sfida_Pag_233
+{
+    class Program
+    {
+        
+        static int catalan(int n)
+        {
+            int res = 0;
+            if (n <= 1)
+            {
+                return 1;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                res += catalan(i)
+                       * catalan(n - i - 1);
+            }
+            return res;
+        }
+
+        public static void Main()
+        {
+            for (int i = 0; i < 10; i++)
+                Console.Write(catalan(i)
+                              + " ");
+            Console.ReadLine();
+        }
+    }
+}
